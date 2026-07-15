14271b7f397d82eb8db0c5353c9006b6152c79c9
Elmer Melendez <melendez.esv@gmail.com>
2026-07-14 23:19:59 -0600

    Estructura de dashboard modular, favicon e iconos png agregados

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,74 @@
-<h1>test</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - ERP</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('css/layout.css') }}">
+</head>
+</head>
+    <main class="erp-menu-container">
+        <div class="erp-menu">
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/estimates.png') }}" alt="estimates">
+                <h4 class="icon-name">Cotización</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/group.png') }}" alt="group">
+                <h4 class="icon-name">Usuarios</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/in-inventory.png') }}" alt="in-inventory">
+                <h4 class="icon-name">Inventario</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/invoice.png') }}" alt="invoice">
+                <h4 class="icon-name">Factura</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/purchase-order.png') }}" alt="purchase-order">
+                <h4 class="icon-name">Orden de Compra</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/report-file.png') }}" alt="report-file">
+                <h4 class="icon-name">Reporte del Sistema</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/sales-report.png') }}" alt="sales-report">
+                <h4 class="icon-name">Reporte de Ventas</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/settings.png') }}" alt="settings">
+                <h4 class="icon-name">Configuracion</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/share.png') }}" alt="share">
+                <h4 class="icon-name">Compartir</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/supplier.png') }}" alt="supplier">
+                <h4 class="icon-name">Proveedor</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/support.png') }}" alt="support">
+                <h4 class="icon-name">Soporte</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/cash-register.png') }}" alt="cash-register">
+                <h4 class="icon-name">Caja Registradora</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/email.png') }}" alt="email">
+                <h4 class="icon-name">Correo</h4>
+            </div>
+            <div class="option">
+                <img class="icon" src="{{ asset('icons/clock.png') }}" alt="clock">
+                <h4 class="icon-name">Reloj</h4>
+            </div>
+        </div>
+    </main>
+    <script src="{{ asset('js/main.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 });
